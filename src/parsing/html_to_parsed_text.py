0d0b69456d1cdea5ee8baf916a9c941f5f2208ee
jamcam11 <32092383+jamcam11@users.noreply.github.com>
2021-10-04 15:46:14 +0100

    Update html_to_parsed_text.py
    improved html parsing esp no refs

--- src/parsing/html_to_parsed_text.py
+++ src/parsing/html_to_parsed_text.py
@@ -1,70 +1,150 @@
 from cadmus.src.parsing.get_attrs import get_attrs
 
 def html_to_parsed_text(soup):
-    
+    import re
+    import unicodedata
     # i want to save the text portions to a holding list, adding new sections each time we think its useful or new.
-    text = []
-    # first we search the body for each division and save it as a list of tag objects
-    if soup.find('body'):
-        div_tags = soup.body.find_all(['div', 'article'])
-        
-        
-        # The most common attribute used for marking a useful division is the "class" or "id" attribute so lets consider them.
-        for tag in div_tags:
-            if tag.name =='article':
-                # if any of these criteria are met then we should look for paragraphs within the div tag
-                paras = tag.find_all(['p', 'div', {'class':'html-p'},'div', {'class':'NLM_paragraph'}])
-                for p in paras:
-                    if p.attrs.get('class') == None\
-                    or 'para' in p.attrs.get('class')\
-                    or 'NLM_paragraph' in p.attrs.get('class')\
-                    or 'html-p' in p.attrs.get('class'):
-                        if p.text.strip() not in text:
-                            text.append(p.text.strip())
-                            
+    text = ''
+    # first we search the body for each division  or article tage and save it as a list of tag object
+
+    # first we look for the cleanest tags.
+    for name in ['article', 'ifp:body', 'articledata']:
+        if soup.find(name):
+            tag = soup.find(name)
+            paras = tag.find_all(['p',
+                                  'h1',
+                                  'h2',
+                                  'h3',
+                                  'div', {'class':'html-p'},
+                                  'div', {'class':'NLM_paragraph'},
+                                  'div', {'class':'NLM_p'},
+                                  'div', {'class':'NLM_p last'}
+                                 ])
+            for p in paras:
+                if p.text.lower() == 'references' \
+                    or p.text.lower() == 'supplementary material' \
+                    or p.text.lower() == 'acknowledgments':
+                    break
+                elif len(p.text.split()) > 1000:
+                    pass
+                
+                else:
+                    attrs = get_attrs(p)
+                    if attrs == [] \
+                    or 'para' in attrs \
+                    or 'html-p' in attrs \
+                    or 'nlm_paragraph' in attrs \
+                    or 'nlm_p' in attrs \
+                    or 'nlm_p last' in attrs \
+                    or 'mb15' in attrs \
+                    or 'mb0' in attrs \
+                    or 'article-section-content' in attrs \
+                    or 'articleparagraph' in attrs:
+                        if p.get_text(" ", strip = True) not in text:
+                            text += p.get_text(" ", strip = True)
+            return text
+            
+            
+    # if we don't find a clean tag we can look through all the tags and check the attributes
+    if text == '':
+        # print('no main tag found')
+        # create a list of tags based on the most useful tag names
+        div_tag = soup.body.find_all(['div', 'section'])
+        # now we have a tag target approach - this tries to identify common tags that hold the neatest representation of the text
+        for tag in div_tag:
+            # use our function to get a flat list of attributest to query
+            attrs_list = get_attrs(tag)
+            # sometimes its empty so check it is not None
+            if attrs_list:
+                # now we have a list of attribute values to query for our common words
+                parse = False
+                for value in attrs_list:
+                    if (('article' in value) and ('body' in value))\
+                    or (('article' in value) and ('fulltext' in value))\
+                    or (('article' in value) and ('content' in value))\
+                    or (("content" in value) and ('body' in value))\
+                    or (('article' in value) and ('row' in value))\
+                    or ('article-full-text' in value)\
+                    or value == 'fulltext'\
+                    or value == 'pagefulltext'\
+                    or value == 'page-body'\
+                    or value == 'article'\
+                    or value == 'fulltext-view'\
+                    or value == 'html-body'\
+                    or value == 'body-text'\
+                    or ('hlfld-fulltext' in value):
+                        
+                        paras = tag.find_all(['p',
+                                              'h2',
+                                              'h3',
+                                              'div', {'class':'html-p'},
+                                              'div', {'class':'NLM_paragraph'},
+                                              'div', {'class':'NLM_p'},
+                                              'div', {'class':'NLM_p last'}
+                                             ])
+                        for p in paras:
+                            #print(p.attrs)
+                            if p.text.lower() == 'references' \
+                                or p.text.lower() == 'supplementary material' \
+                                or p.text.lower() == 'acknowledgments':
+                                #print('stopping due to section break')
+                                break
+                            elif len(p.text.split()) > 1000:
+                                #print("skipping due to size")
+                                pass
+                            else:
+                                attrs = get_attrs(p)
+                                if attrs == [] \
+                                or 'para' in attrs \
+                                or 'html-p' in attrs \
+                                or 'nlm_paragraph' in attrs \
+                                or 'nlm_p' in attrs \
+                                or 'nlm_p last' in attrs \
+                                or 'mb15' in attrs \
+                                or 'mb0' in attrs \
+                                or 'article-section-content' in attrs \
+                                or 'articleparagraph' in attrs:
+                                    if p.get_text(" ", strip = True) not in text:
+                                        text += p.get_text(" ", strip = True)
+        if text != '':
+            return text
+                
+    if text == '':
+        #print('workin on basic tag trawl')
+        # the next step will be extracting the text from our tags 
+        # if any of these criteria are met then we should look for text holding tags
+        paras = soup.body.find_all(['p',
+                                    'h2',
+                                    'h3',
+                                    'div', {'class':'html-p'},
+                                    'div', {'class':'NLM_paragraph'},
+                                    'div', {'class':'NLM_p'},
+                                    'div', {'class':'NLM_p last'}
+                                   ])
+        for p in paras:
+            if p.text.lower() == 'references' \
+                or p.text.lower() == 'supplementary material' \
+                or p.text.lower() == 'acknowledgments':
+                break
+            elif len(p.text.split()) > 1000:
+                pass
             else:
-                # we can use the function above to get the values from the attributes for each tag
-                attrs_list = get_attrs(tag)
-                if attrs_list:
-                    # now we have a list of attribute values to query for our common words
-                    for value in attrs_list:
-                        if (('article' in value) and ('body' in value))\
-                        or (('article' in value) and ('fulltext' in value))\
-                        or (('article' in value) and ('content' in value))\
-                        or (("content" in value) and ('body' in value))\
-                        or ('article-full-text' in value)\
-                        or value == 'fulltext'\
-                        or value == 'article'\
-                        or value == 'fulltext-view'\
-                        or value == 'html-body'\
-                        or ('hlfld-fulltext' in value):
-                            # if any of these criteria are met then we should look for paragraphs within the div tag
-                            paras = tag.find_all(['p', 'div', {'class':'html-p'}, 'div', {'class':'NLM_paragraph'}])
-                            for p in paras:
-                                if p.attrs.get('class') == None\
-                                or 'para' in p.attrs.get('class')\
-                                or 'html-p' in p.attrs.get('class')\
-                                or 'NLM_paragraph' in p.attrs.get('class'):
-                                    if p.text.strip() not in text:
-                                        text.append(p.text.strip())
-                                        
-                                       
-        text = ' '.join(text)
-        if text == '':
-            text = []
-            for tag in div_tags:
-                # if any of these criteria are met or return no text then we should look for paragraphs within the generic div tag
-                paras = tag.find_all('p')
-                for p in paras:
-                    if p.attrs.get('class') == None\
-                    or 'para' in p.attrs.get('class')\
-                    or 'NLM_paragraph' in p.attrs.get('class')\
-                    or 'html-p' in p.attrs.get('class'):
-                        if p.text.strip() not in text:
-                            text.append(p.text.strip())
-            text = ' '.join(text)
-            if text == '':
-                text = None
-                           
-        
+                attrs = get_attrs(p)
+                if attrs == [] \
+                or 'para' in attrs \
+                or 'html-p' in attrs \
+                or 'nlm_paragraph' in attrs \
+                or 'nlm_p' in attrs \
+                or 'mb15' in attrs \
+                or 'mb0' in attrs \
+                or 'article-section-content' in attrs \
+                or 'articleparagraph' in attrs:
+                    if p.get_text(" ", strip = True) not in text:
+                        text += p.get_text(" ", strip = True)
+
+    # finally we want to check remove the [, , , ] from stripping out references etc
+    text = re.sub('\[[, ]*\]', '', text)
+    # remove any of the unicode characeters and \n chars
+    text = unicodedata.normalize("NFKD", text).replace('\n', '')
+
     return text
