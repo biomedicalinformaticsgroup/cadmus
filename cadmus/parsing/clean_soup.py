6b9e8b87701ef69d50576bb198862fa1d128bc61
jcampb14 <jcampb14@ed.ac.uk>
2021-10-28 20:41:52 +0000

    html parse, split joined paragraphs

--- cadmus/parsing/clean_soup.py
+++ cadmus/parsing/clean_soup.py
@@ -2,6 +2,7 @@ def clean_soup(soup):
     # here is a list of tags that i want to remove before i extract the text
     # these include links, scripts, supplemnetal tags, references
     tag_remove_list = ['a',
+                       'button',
                        'script',
                        'xref',
                        'table',
@@ -25,6 +26,20 @@ def clean_soup(soup):
                        'ul',
                        'li'
                       ]
+    
+    button_tags = soup.find_all('button')
+    for tag in button_tags:
+        if tag.parent:
+            tag.parent.decompose()
+    shares = soup.find_all("div", class_="share-access hidden")
+    for tag in shares:
+        tag.decompose()
+    shares = soup.find_all("div", id_="share_Pop")
+    for tag in shares:
+        tag.decompose() 
+    shares = soup.find_all("div", class_="shareable")
+    for tag in shares:
+        tag.decompose()    
     # find all the tags matching any name in the list
     tags = soup.find_all(tag_remove_list)
     # loop through each tag and apply the decompose function to remove them from the soup
