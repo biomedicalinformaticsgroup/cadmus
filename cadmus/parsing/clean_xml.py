8ad1da221f41d135149e448833c4b967d7529c6b
jcampb14 <jcampb14@ed.ac.uk>
2021-10-28 21:26:32 +0000

    html, xml clean up scripts

--- cadmus/parsing/clean_xml.py
+++ cadmus/parsing/clean_xml.py
@@ -10,12 +10,23 @@ def clean_xml(p_text):
         p_text = p_text.replace('https://','')
         p_text = p_text.replace('http://','')
         p_text = p_text.replace('doi:','')
-        p_text = p_text.replace('ftp:','')
+        p_text = p_text.replace('ftp:','')              
+        p_text = p_text.replace('×','x')
+        p_text = p_text.replace('®','')
+        p_text = p_text.replace('≤','<=')
+        p_text = p_text.replace('≥','>=')
+        p_text = p_text.replace('≥','>=')
+        p_text = p_text.replace('…','.')
+        p_text = p_text.replace('°',' degrees')
+        p_text = p_text.replace('±',' +/-')
+        p_text = p_text.replace('\n', '')
+        p_text = p_text.replace('\t', '')
         # remove the email adresses of the text
         email_detection = re.compile('\w+@\w+\.[a-z]{3}')
         result = re.findall(email_detection, p_text)
         for i in range(len(result)):
             p_text = p_text.replace(result[i],'')
 
-        p_text = p_text.replace('  ',' ')          
+        p_text = p_text.replace('  ',' ')
+
     return p_text
