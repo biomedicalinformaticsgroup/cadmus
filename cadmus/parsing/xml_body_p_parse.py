3b021fca56a18abca9f7d683e32b944c133ded9f
s2010515 <s2010515@ed.ac.uk>
2021-11-04 14:54:22 +0000

    Update clean up 1) plain 2) pdf and fix xml_parse_d

--- cadmus/parsing/xml_body_p_parse.py
+++ cadmus/parsing/xml_body_p_parse.py
@@ -17,11 +17,15 @@ def xml_body_p_parse(soup, abstract):
                     p_text.extend([p.text.strip() for p in ps if p.text.strip() not in p_text])
             # join each p element with a space 
             p_text = ' '.join(p_text)
+        else:
+            # when there is no body tag then the XML is not useful.
+            print('No Body, looks like a False Positive XML or Abstract Only')
+            p_text = ''
     else:
         # when there is no body tag then the XML is not useful.
         print('No Body, looks like a False Positive XML or Abstract Only')
         p_text = ''
     # ensure the p_text is a simplified string format even if the parsing fails    
-    if p_text == [] or p_text == ' ':
+    if p_text == [] or p_text == '' or p_text == ' ':
         p_text = ''
     return p_text
